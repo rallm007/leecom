6b4b599be8a68c3957051f83f37c7bcc4c041559
tonik-tong <tongnaike@126.com>
2015-05-22 02:45:35 +0800

    add new files

---
+++ cn/Products/Aquariums/ajax-content/xk250.php
@@ -0,0 +1,100 @@
+<div class="tabs content-tabs full ui-tabs ui-widget ui-widget-content ui-corner-all">
+    <ul class="clearfix ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all quantity-3">
+        <li class="ui-state-default ui-corner-top ui-tabs-selected ui-state-active"><a href="#tabs-1">产品详情</a></li>
+        <li class="ui-state-default ui-corner-top"><a href="#tabs-2">技术参数</a></li>
+    </ul>
+    <div id="tabs-1" class="ui-tabs-panel ui-widget-content ui-corner-bottom">
+            
+
+            <div class="tab-teaser-prod_details top clearfix">
+                <div class="grid_8">
+                    <h2>XK-250方寸之间，一个晶莹剔透的世界</h2>
+                    <p>方寸之间呈现的是一个立方体的美丽图画，养的不仅仅是欢快的鱼与茂盛的草，更是美丽的风景与愉悦的心情。整体设计大方，高雅，淡雅的色调酝酿出一种低调的奢华，无论做工或设计，处处流露超凡的高水准,每一个边角都由顶级的工程塑胶原料制造，开关防水帽是你24小时不下班的安全卫士。 可替换滤材的上部过滤器令您的水质永远保持最洁净的状态。</p>
+                </div>
+                <!-- <div class="grid_4">
+                    <img src="../../../../out/pictures/generated/srcms/1/618_342_80/01300(19).jpg" alt="">
+                </div> -->
+            </div>
+                        
+            
+
+            
+                        
+            
+
+            <div id="filter" class="tab-teaser-prod_details clearfix">
+                <div class="grid_8">
+                    <h3 class="icon-headline filter-heater">合二为一的生化过滤系统和节能照明系统</h3>
+                    <p>节能环保的高亮度LED灯，想开就开想关就关，绝然不影响生化过滤系统的正常运作，过滤、增氧、照明系统的浑然一体令您的爱鱼享有更多的活动空间，满足您简约时尚的品味。<br></p>
+                    
+                </div>
+                <!-- <div class="grid_4">
+                    <img src="../../../../out/pictures/generated/srcms/1/618_342_80/01300(2).jpg" alt="">
+                </div> -->
+            </div>
+                        
+        
+    </div>
+    <div id="tabs-2" class="ui-tabs-panel ui-widget-content ui-corner-bottom ui-tabs-hide">
+<div class="tab-teaser-technical clearfix">
+    <!-- <div class="image">
+                    <img src="../../../../out/pictures/generated/srcms/1/618_342_80/00010.jpg" alt="">
+            </div> -->
+    <table>
+                                <thead>
+                    <tr>
+                        <td colspan="3">XK-200迷你鱼缸</td>
+                    </tr>
+                </thead>
+                                <tbody>                              
+                    <tr>
+                        <td class="icon produkt">&nbsp;</td>
+                        <td class="topic">产品名称</td>
+                        <td>XK-200迷你鱼缸</td>
+                    </tr>
+                    <tr>
+<td class="icon dekore">&nbsp;</td>
+<td class="topic">
+                        颜色</td>
+<td>
+<ul class="decor fl">
+<li><img alt="" src="../../../../out/pictures/master/global/1/4.png"></li></ul></td></tr>
+<tr>
+                        <td class="icon watt">&nbsp;</td>
+                        <td class="topic">LED灯功率</td>
+                        <td>3 W</td>
+                    </tr>
+                        <tr>
+                        <td class="icon filtervolumen">&nbsp;</td>
+                        <td class="topic">装箱数</td>
+                        <td>2 PCS</td>
+                    </tr>
+                 <tr>
+                        <td class="icon measure">&nbsp;</td>
+                        <td class="topic">规格</td>
+                        <td>255 x 255x 310 mm</td>
+                    </tr>
+                                                                                                                                                                        
+                    <tr>
+                        <td class="icon gewicht">&nbsp;</td>
+                        <td class="topic">重量</td>
+                        <td>6 Kg</td>
+                    </tr>
+                                                                        
+                    <tr>
+                        <td class="icon liter">&nbsp;</td>
+                        <td class="topic">容量</td>
+                        <td>18 L</td>
+                    </tr>
+                    <tr>
+                        <td class="icon energieverbrauch">&nbsp;</td>
+                        <td class="topic">水泵功率</td>
+                        <td>3 W</td>
+                    </tr>
+                    </tbody>
+    </table>
+</div>
+                        
+    </div>
+
+    </div>
